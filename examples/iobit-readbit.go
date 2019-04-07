c27410f15ff60b94fafc846562034e3bbd1ee3e7
Yoshihiro Yamazaki <yoya@awm.jp>
2019-04-08 02:33:48 +0900

    read bits command

---
+++ examples/iobit-readbit.go
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2019/04/08- yoya@awm.jp. All rights reserved.
+ */
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/yoya/go-iobit/iobit"
+	"io"
+	"os"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		buffio := bytes.NewBufferString(arg)
+		iob := iobit.NewReader(buffio, iobit.BigEndian)
+		for {
+			v, err := iob.GetUIBit()
+			if err != nil {
+				if err != io.EOF {
+					fmt.Println(err)
+				}
+				break
+			}
+			fmt.Printf("%x ", v)
+			_, bitOff := iob.GetOffset()
+			if bitOff == 0 {
+				fmt.Printf(" ")
+			}
+		}
+		fmt.Println("")
+	}
+
+}
